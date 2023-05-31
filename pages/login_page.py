e796f3f7e2279c38a6e96a17721ee87ff00e7ec9
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-05-31 11:35:10 +0300

    add test_login_page

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+
+
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import LoinPageLocators, MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+        self.url = self.browser.current_url
+        assert "login" in self.url, "url is not activ"
+   
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoinPageLocators.LOGIN_FORM), "Login_form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoinPageLocators.REGISTER_FORM), "Register_form is not presented"
